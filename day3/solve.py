1ef2b7e9e0916cfbda5fbf31a8110d51454af214
David Long <david.wip.long@gmail.com>
2022-12-30 14:19:46 +0800

    complete day3

--- day3/solve.py
+++ day3/solve.py
@@ -30,19 +30,56 @@ def solve(lines):
           break
     print('found:', founditem)
           
-  print('sum:', priosum)
+  print('\n\n---------\nsum:', priosum)
 
 
+def solve2(lines):
+  linecount = len(lines)
+  print(linecount, 'lines:')
+  print(''.join(lines),end='')
+  print('----------------')
+  
+  priosum = 0
+  groupsum = 0
+
+  curline = 0 # line index
+
+  groupsize = 3
+
+  while curline <= linecount - groupsize:
+    line = lines[curline]
+    l = len(line)
+
+    # find common group
+    line1, line2, line3 = lines[curline], lines[curline+1], lines[curline+2]
+    print('\n-------\ngroups:\n', line1, line2, line3, end='\n')
+    foundgroup = ''
+
+    for i1 in line1:
+      if foundgroup != '': break
+
+      for i2 in line2:
+        if foundgroup != '': break
+
+        for i3 in line3:
+          if i1 == i2 == i3:
+            foundgroup = i1
+            groupsum += ii(foundgroup)
+            print('found group:', foundgroup, ii(foundgroup), end='\n\n')
+            break
+    
+    curline += groupsize
+
+  print('sum:', groupsum)
+  return groupsum
 
 
-
-
+print('------------------------')
 
 testinput = loadfile("input.txt")
 #print(testinput)
-solve(testinput)
+#solve(testinput)
+solve2(testinput)
 
 
-print(ii('a'))
-print(ii('Z'))
 
